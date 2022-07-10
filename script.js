717abb9df6f683dc25ed32e752183c1b4622a113
GABRIEL SOUZA LEANDRO <101406880+GabrielSouzaLeandro@users.noreply.github.com>
2022-07-10 18:13:12 -0300

    inicial

---
+++ script.js
@@ -0,0 +1,10 @@
+ const peso1 = 1
+ const peso2 = 2
+
+
+const avaliacao1 = 9.09998
+
+const avaliacao2 = 7.98737  
+const total = avaliacao1 * peso1 + avaliacao2 * peso2 
+const media = total / (peso1 + peso2)
+console.log(media. toFixed(2))
